c58c12d696407980fd97f6554616efb6f4b807c2
gianlucat973 <100386271+gianlucat973@users.noreply.github.com>
2022-02-25 15:06:53 +0100

    Update startBackEnd.sh

--- startBackEnd.sh
+++ startBackEnd.sh
@@ -1,1 +1,8 @@
 echo "inserisci qui i comandi per lo starting del Back-End della tua applciazione web"
+cd /home/runner/work/Tesi-StrumentoGenerale/Tesi-StrumentoGenerale/insert-here-your-web-app/angular-java-example-master
+mvn clean install
+cd /home/runner/work/Tesi-StrumentoGenerale/Tesi-StrumentoGenerale/insert-here-your-web-app/angular-java-example-master/target
+echo "Vediamo quali file jar si trovano in cartella target"
+ls -a
+echo "Proviamo ad eseguire ${{ secrets.NOME_JAR_WEBAPP }}.jar"
+java -jar users-0.0.1-SNAPSHOT.jar
