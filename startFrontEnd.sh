5804150265e39f0cf1eb3dd0ae06a2f2325597cb
gianlucat973 <100386271+gianlucat973@users.noreply.github.com>
2022-02-25 15:07:22 +0100

    Update startFrontEnd.sh

--- startFrontEnd.sh
+++ startFrontEnd.sh
@@ -1,1 +1,6 @@
 echo "inserisci qui i comandi per lo starting del Front-End della tua applciazione web"
+cd /home/runner/work/Tesi-StrumentoGenerale/Tesi-StrumentoGenerale/insert-here-your-web-app/angular-java-example-master/src/main/ui
+echo "Siamo nella directory FE, proviamo a lanciarlo in esecuzione"
+npm install
+echo "Installazione npm effettuata, prossimo comando: npm start"
+npm start
