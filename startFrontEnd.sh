232105c711049847ca8b469859b615abdd74f9f2
gianlucat973 <100386271+gianlucat973@users.noreply.github.com>
2022-03-03 23:00:09 +0100

    Update startFrontEnd.sh

--- startFrontEnd.sh
+++ startFrontEnd.sh
@@ -1,4 +1,25 @@
 echo "inserisci qui i comandi per lo starting del Front-End della tua applciazione web"
+
+echo "Inizio comandi installazione precondizioni"
+sudo apt update
+sudo apt -y install curl dirmngr apt-transport-https lsb-release ca-certificates
+curl -sL https://deb.nodesource.com/setup_10.x | sudo bash
+sudo apt install nodejs
+sudo npm cache clean -f
+sudo npm install -g n
+sudo n stable
+sudo n 10.18.0        
+echo "Versione di node: "
+node -v
+echo "Versione di npm: "
+npm -v          
+npm install
+echo "Provo ad INSTALLARE bcrypt"
+npm install bcrypt
+npm fund
+echo "Fine comandi installazione Node"
+
+echo "Inizio comandi esecuzione Frontend"
 cd /home/runner/work/Tesi-StrumentoGenerale/Tesi-StrumentoGenerale/insert-here-your-web-app/angular-java-example-master/src/main/ui
 echo "Siamo nella directory FE, proviamo a lanciarlo in esecuzione"
 npm install
