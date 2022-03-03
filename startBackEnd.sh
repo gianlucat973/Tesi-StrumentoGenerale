bbf3883a9ea37983b1373150900423f367ba9508
gianlucat973 <100386271+gianlucat973@users.noreply.github.com>
2022-03-03 23:17:50 +0100

    Update startBackEnd.sh

--- startBackEnd.sh
+++ startBackEnd.sh
@@ -1,12 +1,11 @@
 echo "inserisci qui i comandi per lo starting del Back-End della tua applicazione web"
 sudo apt update
-sudo apt install openjdk-8-jdk openjdk-8-jre
+sudo apt install openjdk-11-jdk openjdk-11-jre
 echo "Installata versione di java numero"
 java -version 
-cd /home/runner/work/Tesi-StrumentoGenerale/Tesi-StrumentoGenerale/insert-here-your-web-app/angular-java-example-master
+cd /home/runner/work/Tesi-StrumentoGenerale/Tesi-StrumentoGenerale/insert-here-your-web-app/root/backend
 mvn clean install
-cd /home/runner/work/Tesi-StrumentoGenerale/Tesi-StrumentoGenerale/insert-here-your-web-app/angular-java-example-master/target
+cd /home/runner/work/Tesi-StrumentoGenerale/Tesi-StrumentoGenerale/insert-here-your-web-app/root/backend/target
 echo "Vediamo quali file jar si trovano in cartella target"
 ls -a
-echo "Proviamo ad eseguire ${{ secrets.NOME_JAR_WEBAPP }}.jar"
-java -jar users-0.0.1-SNAPSHOT.jar
+java -jar backend-0.0.1-SNAPSHOT.jar
