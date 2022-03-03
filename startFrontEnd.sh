d4134bd391846413faf72c4bc92669bae8977d40
gianlucat973 <100386271+gianlucat973@users.noreply.github.com>
2022-03-04 00:16:27 +0100

    Update startFrontEnd.sh

--- startFrontEnd.sh
+++ startFrontEnd.sh
@@ -1,31 +1,18 @@
 echo "inserisci qui i comandi per lo starting del Front-End della tua applciazione web"
 
 echo "Inizio comandi installazione precondizioni"
-sudo apt update
-sudo apt -y install curl dirmngr apt-transport-https lsb-release ca-certificates
-curl -sL https://deb.nodesource.com/setup_12.x | sudo bash
+cd /home/runner/work/Tesi-StrumentoGenerale/Tesi-StrumentoGenerale
+curl -sL https://deb.nodesource.com/setup_12.x -o nodesource_setup.sh
+cat nodesource_setup.sh
+sudo bash nodesource_setup.sh
 sudo apt install nodejs
-sudo npm cache clean -f
-sudo npm install -g n
-sudo n stable
-sudo n 12.20       
-echo "Versione di node: "
+echo "versione di node installata"
 node -v
-echo "Versione di npm: "
-npm -v
-echo "Inizio init di npm"
-npm init
-echo "Inizio install di npm"
-npm install
-echo "Provo ad INSTALLARE bcrypt"
-npm install bcrypt
-npm fund
+echo "Installazione di npm"
+sudo apt install npm
 echo "Fine comandi installazione Node"
 
 echo "Inizio comandi esecuzione Frontend"
-cd /home/runner/work/Tesi-StrumentoGenerale/Tesi-StrumentoGenerale
-echo "Inizio init di npm"
-npm init
 cd /home/runner/work/Tesi-StrumentoGenerale/Tesi-StrumentoGenerale/insert-here-your-web-app/angular-java-example-master/root/frontend
 echo "Siamo nella directory FE, proviamo a lanciarlo in esecuzione"
 echo "Inizio init di npm"
