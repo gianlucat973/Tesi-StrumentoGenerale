3f86ea8d2618d3163eea91aba881a38dcb078ec4
gianlucat97 <84741536+gianlucat97@users.noreply.github.com>
2022-03-04 02:36:44 +0100

    Aggiunta file di test per verificare che funzioni correttamente mainOnPush dopo le ottimizzazioni

---
+++ project-test-headless/src/test/java/com/example/TesiIntegrazioneProgettoEsterno/TestHooks3.java
@@ -0,0 +1,79 @@
+package com.example.TestSuite3;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.apache.commons.io.FileUtils;
+import java.io.File;
+
+public class TestHooks3 {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+  JavascriptExecutor js;
+  @Before
+  public void setUp() throws Exception {
+    System.setProperty("webdriver.chrome.driver", "");
+    driver = new ChromeDriver();
+    baseUrl = "https://www.google.com/";
+    driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+    js = (JavascriptExecutor) driver;
+  }
+
+  @Test
+  public void testHooks_loc_Hooks_release_1_2() throws Exception {
+    driver.get("http://localhost:4200/");
+    driver.findElement(By.xpath("//*[@x230953393589-x-test-tpl-1]//*[@x230953393589-x-test-hook-9]//*[@x230938714303-x-test-tpl-2]//*[@x230938714303-x-test-hook-13]")).click();
+    driver.findElement(By.xpath("//*[@x230953393589-x-test-tpl-1]//*[@x230953393589-x-test-hook-9]//*[@x230938714303-x-test-tpl-2]//*[@x230938714303-x-test-hook-13]")).clear();
+    driver.findElement(By.xpath("//*[@x230953393589-x-test-tpl-1]//*[@x230953393589-x-test-hook-9]//*[@x230938714303-x-test-tpl-2]//*[@x230938714303-x-test-hook-13]")).sendKeys("Perretta");
+    driver.findElement(By.xpath("//*[@x230953393589-x-test-tpl-1]//*[@x230953393589-x-test-hook-9]//*[@x230938714303-x-test-tpl-2]//*[@x230938714303-x-test-hook-6]")).click();
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
---
+++ project-test-headless/src/test/java/com/example/TesiIntegrazioneProgettoEsterno/TestTradizionale3.java
@@ -0,0 +1,79 @@
+package com.example.TestSuite3;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.apache.commons.io.FileUtils;
+import java.io.File;
+
+public class TestTradizionale3 {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+  JavascriptExecutor js;
+  @Before
+  public void setUp() throws Exception {
+    System.setProperty("webdriver.chrome.driver", "");
+    driver = new ChromeDriver();
+    baseUrl = "https://www.google.com/";
+    driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+    js = (JavascriptExecutor) driver;
+  }
+
+  @Test
+  public void testTradizionale_release_1_2() throws Exception {
+    driver.get("http://localhost:4200/");
+    driver.findElement(By.id("exampleInputPassword1")).click();
+    driver.findElement(By.id("exampleInputPassword1")).clear();
+    driver.findElement(By.id("exampleInputPassword1")).sendKeys("Perretta");
+    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Create User'])[1]/following::form[1]")).click();
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
